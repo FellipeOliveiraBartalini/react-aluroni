e81be936f8879174066f65bb28ef4d1276486113
Fellipe de Oliveira <fellipeoliveiranobre@hotmail.com>
2022-08-09 09:35:48 -0300

    Criado component cardapio

---
+++ src/pages/cardapio/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../../components/button';
+import style from './Cardapio.module.scss';
+
+function Cardapio() {
+    return (
+        <main>
+            <nav className='menu'></nav>
+            <Button>Button</Button>
+        </main>
+    )
+}
+
+export default Cardapio
