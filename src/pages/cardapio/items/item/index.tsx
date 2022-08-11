8428131368adddf24259ad0fd3a36cd5a86db5ab
Fellipe de Oliveira <fellipeoliveiranobre@hotmail.com>
2022-08-11 10:12:52 -0300

    add style baseado na categoria do item

--- src/pages/cardapio/items/item/index.tsx
+++ src/pages/cardapio/items/item/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Item.module.scss';
 import cardapio from '../items.json';
+import classNames from 'classnames';
 
 type Props = typeof cardapio[0];
 
@@ -17,7 +18,10 @@ export default function Item(item: Props) {
                     <p>{item.description}</p>
                 </div>
                 <div className={style.item__tags}>
-                    <div className={style.item__tipo}>
+                    <div className={classNames({
+                        [style.item__tipo]: true,
+                        [style[`item__tipo__${item.category.label.toLowerCase()}`]]: true
+                    })}>
                         {item.category.label}
                     </div>
                     <div className={style.item__porcao}>
