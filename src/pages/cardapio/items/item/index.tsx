1c42386e1d32977ca8c3b6d76b1f906ffa59e443
Fellipe de Oliveira <fellipeoliveiranobre@hotmail.com>
2022-08-11 10:01:19 -0300

    Populando os itens

--- src/pages/cardapio/items/item/index.tsx
+++ src/pages/cardapio/items/item/index.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import style from './Item.module.scss';
+import cardapio from '../items.json';
 
-export default function Item() {
+type Props = typeof cardapio[0];
+
+export default function Item(item: Props) {
     return (
         <div className={style.item}>
             <div className={style.item__imagem}>
-                <img src={item.photo} alt="imagem" />
+                <img src={item.photo} alt={"Foto do "+ item.title} />
             </div>
 
             <div className={style.item__descricao}>
                 <div className={style.item__titulo}>
-                    <h2>{Item.title}</h2>
+                    <h2>{item.title}</h2>
                     <p>{item.description}</p>
                 </div>
                 <div className={style.item__tags}>
                     <div className={style.item__tipo}>
-                        {item.tipo}
+                        {item.category.label}
                     </div>
                     <div className={style.item__porcao}>
-                        {item.size}
+                        {item.size}g
                     </div>
                     <div className={style.item__qtdpessoas}>
-                        {item.serving}
+                        {item.serving} pessoa{item.serving === 1 ? '' : 's'}
                     </div>
                     <div className={style.item__valor}>
-                        {item.price}
+                        R$ {item.price.toFixed(2)}
                     </div>
                 </div>
             </div>
